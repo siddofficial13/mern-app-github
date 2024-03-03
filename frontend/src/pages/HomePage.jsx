43628522e467d0f3dc48a349dd5ffaa11dd22c7b
Siddharth <siddofficial13@gmail.com>
2024-03-03 22:59:07 +0530

    api routes handled

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -20,17 +20,11 @@ const HomePage = () => {
   const getUserProfileAndRepos = useCallback(async(username="siddofficial13") => {
     setLoading(true);
     try{
-        const userRes = await fetch(`https://api.github.com/users/${username}`, {
-          headers:{ 
-            authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`}
-        });
-        const userProfile = await userRes.json();
-        setUserProfile(userProfile);
-
-        const repoRes = await fetch(userProfile.repos_url);
-        const repos = await repoRes.json();
+       const res = await fetch(`http://localhost:5000/api/users/profile/${username}`); 
+       const { repos, userProfile } = await res.json();
         repos.sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
         setRepos(repos);
+        setUserProfile(userProfile);
         // setLoading(false);
         console.log(userProfile);
         console.log(repos);
